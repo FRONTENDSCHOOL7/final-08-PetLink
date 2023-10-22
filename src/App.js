5822527eebaa55c751b650d6c00eb35f864b19c2
maskelog <blitzmaske@gmail.com>
2023-10-22 19:36:31 +0900

    :memo: [Modify] 로그인페이지 이동기능추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
+import MainLoginPage from "./Components/LoginPage/MainLoginPage"; 
 import JoinPage from "./Components/JoinPage/JoinPage";
-import LoginPage from "./Components/LoginPage/LoginPage_dev";
-import ProfilePage from "./Components/ProfilePage/ProfilePage";
+import LoginPage from "./Components/LoginPage/LoginPage";
+import ProfilePage from "./Components/ProfilePage/ProfilePage"
 
 function App() {
-    const [page, setPage] = useState('login');
+    const [page, setPage] = useState('mainLogin');
     const [userInfo, setUserInfo] = useState(null); // userInfo 상태 추가
 
     const handlePage = (newPage) => {
@@ -33,9 +34,10 @@ function App() {
             <button type="button" onClick={() => handlePage('profile')}>Profile Page</button>
             {userInfo && JSON.stringify(userInfo)}
 
+            {page === 'mainLogin' && <MainLoginPage handlePage={handlePage} />}
             {page === 'login' && <LoginPage handlePage={() => handlePage('join')} />}
             {page === 'join' && <JoinPage handlePage={() => handlePage('login')} />}
-            {page === 'profile' && <ProfilePage userInfo={userInfo} />} {/* userInfo prop을 전달 */}
+            {page === 'profile' && <ProfilePage userInfo={userInfo} />}
         </div>
     );
 }
