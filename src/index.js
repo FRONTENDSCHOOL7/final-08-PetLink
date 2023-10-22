29ad6696ba3515326c9f2a9e083567439f9f7541
sunny <sunnya2023@gmail.com>
2023-10-23 03:26:09 +0900

    :construction: [Ing] 포스트 댓글 화면 구현 중

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 // import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import App1 from "./App1";
+
 
 import './index.css'
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(<App1 />);
