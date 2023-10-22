9131bc67250d8490300b1a6f8f85bb31a06d109f
sunny <sunnya2023@gmail.com>
2023-10-22 19:46:35 +0900

    📝 [Modify] : 네브바, 홈페이지 생성

--- src/Components/Common/NavBar.jsx
+++ src/Components/Common/NavBar.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
-
+import './navBar.css'
+import icoHome from '../../assets/images/icon-home.png'
+import icoCommunity from '../../assets/images/icon-community.png'
+import icoMarket from '../../assets/images/icon-market.png'
+import icoChatting from '../../assets/images/icon-chatting.png'
+import icoUser from '../../assets/images/icon-user.png'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 export default function NavBar() {
   return (
-    <div>NavBar</div>
+    <nav className='nav'>
+S          <a href="/" className='nav-link active'>
+            <img src={icoHome} />
+            <span className='nav-text'>홈</span>
+          </a>
+          <a href="#" className='nav-link '>
+            <img src={icoCommunity} />
+            <span className='nav-text'>커뮤니티</span>
+          </a>
+          <a href="#" className='nav-link'>
+            <img src={icoMarket} />
+            <span className='nav-text'>반결장터</span>
+          </a>
+          <a href="#" className='nav-link'>
+            <img src={icoChatting} />
+            <span className='nav-text'>채팅</span>
+          </a>
+          <a href="#" className='nav-link'>
+            <img src={icoUser} />
+            <span className='nav-text'>프로필</span>
+          </a>
+
+        </nav>
   )
 }
--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,7 +1,130 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './homePage.css'
+import icoMore from '../../assets/images/icon- more-vertical.png'
+import icoProfile from '../../assets/images/icon-basic-profile.png'
+import icoHeart from '../../assets/images/icon-heart.png'
+import icoRedHeart from '../../assets/images/icon-red-heart.png'
+import icoComment from '../../assets/images/icon-comment.png'
+import logoTxt from '../../assets/images/logo-color_txt.png'
+import icoSearch from '../../assets/images/icon-search.png'
+import icoBack from '../../assets/images/icon-arrow-left.png'
+
 
 export default function HomePage() {
+
+  const [likeNum, setLikeNum] = useState(0)
+  const [modal, setShowModal] = useState(false)
+
+
+const onChangeNum = ()=>{
+setLikeNum(likeNum+1)
+}
+
+const onChangeModalShow = ()=>{
+  setShowModal(true)
+}
   return (
-    <div>HomePage</div>
+<>
+        <div className='home-post-wrapp'>
+    {/* header component 하기 */}
+        <header className='home-nav-top'>
+          <img src={logoTxt} alt='반결고리'/>
+          <a href="#"><img src={icoSearch} aria-label='검색하기'/></a>
+        </header>
+
+        
+    {/* Post content component 하기 */}
+    <div className='post-container'>
+      <ul className='post-list'> 
+          <li className='post-item'>
+             <div className='user-info'>
+                <div className="user-profile">
+                  <a href='#'><img src={icoProfile} alt='사용자 프로필 이미지'/></a>
+                  <div className="user-detail">
+                      <span className='title'>애월읍에서 강아지들에게 유명한 곳</span>
+                      <span className='user-id'> @활동명</span>
+                  </div> 
+                </div>
+                <button onClick={onChangeModalShow} ><img className="icon-more" src={icoMore} alt='신고하기 모달창 불러오기'/></button>
+             {
+              modal == true ? <ComplainModal/> :null      
+             }
+             </div>
+       
+          
+        <div className='content'> 
+          <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore tenetur quaerat ut fugit sequi. Temporibus illo nihil facere tempora deserunt?</p>
+          <img src="https://via.placeholder.com/304x228" alt="포스팅 이미지"  />
+          
+          <div className='post-icons'>
+              <button aria-label='좋아요 누르기' onClick={onChangeNum}>
+                <img src={icoRedHeart} alt='하트 아이콘'/>
+                <span>{likeNum}</span>
+              </button>
+              <button aria-label='댓글 남기기' >
+                <img src={icoComment} alt='채팅 아이콘'/>
+              <span>12</span>
+              </button>
+          </div>
+          <div className='post-date'><span>2023년 10월 21일</span></div>
+        </div>
+         {/* <CommentList/>
+         <Comment/> */}
+         </li>
+      </ul>
+    </div>
+        </div>
+</>
+
+  )
+}
+
+{/* 신고하기 모달 */}
+function ComplainModal (){
+      <div className='bottomsheet-complain'>
+            <div className='bar'></div>
+            <span >신고하기</span>
+       </div>
+}
+
+function NavHeaderSearch(){
+  return(
+    <div>
+    <header className='home-nav-top'>
+      <img src={icoBack} alt='뒤로가기'/>
+      <a href="#"><img src={icoSearch} aria-label='검색하기'/></a>
+    </header>
+</div>
+  )
+}
+
+function CommentList(){
+ return(
+ <div className='comment-list'>
+    <div className='user-info'>
+                  <div className="user-profile">
+                    <a href='#'><img src={icoProfile} alt='사용자 프로필 이미지'/></a>
+                    <div className="user-detail">
+                        <span className='user-id'> @활동명</span>
+                    </div> 
+                  </div>
+                  <button  ><img className="icon-more" src={icoMore} alt='신고하기 모달창 불러오기'/></button>
+    </div>
+   <div className='content'> 
+     <p className='text-comment'>댓글입니다. </p>
+   </div>
+ </div>
+ )
+}
+
+function Comment(){
+  return(
+    <div className='comment'>
+           <input type="text" id="commemt-input" placeholder="댓글 입력하기..."/>
+           <div class="img-cover">
+              <img class="profile-img" src={icoProfile} alt="사용자 프로필"/>
+                </div>
+            <button type="submit" class="btn-comment" disabled>게시</button>
+    </div>
   )
 }
