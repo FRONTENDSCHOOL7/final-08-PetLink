997a40e11b8d8f5802a5d8bdaa83bb19a554ceb6
maskelog <blitzmaske@gmail.com>
2023-10-24 01:28:22 +0900

    :sparkles: [Add] 라우트 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import ChatPage from './Pages/ChatPage/Chat';
-import LoginPage from './Pages/LoginPage/Login';
-import JoinPage from './Pages/JoinPage/Join';
-import CommunityPage from './Pages/CommunityPage/Community';
-import HomePage from './Pages/HomePage/Home';
-import MarketPage from './Pages/MarketPage/Market';
-import ProfilePage from './Pages/ProfilePage/Profile';
-import FollowPage from './Pages/FollowPage/FollowList';
-import ErrorPage from './Pages/ErrorPage/Error404';
-import SplashPage from './Pages/Splashpage/Splash';
+import Router from './Router/Router';
+import { GlobalStyle } from "./Styles/reset.style";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SplashPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/join" element={<JoinPage />} />
-        <Route path="/community" element={<CommunityPage />} />
-        <Route path="/market" element={<MarketPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/follow" element={<FollowPage />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+    <GlobalStyle />
+    <Router />
+    </>
   );
 }
 
