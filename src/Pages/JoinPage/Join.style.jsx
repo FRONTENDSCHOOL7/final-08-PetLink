1b915a4709da99cb15f787bf2c93fa594dc2cbaf
maskelog <blitzmaske@gmail.com>
2023-10-24 10:18:20 +0900

    :sparkles: [Add] 스플레시페이지 추가

--- src/Pages/JoinPage/Join.style.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Join.style() {
-  return (
-    <div>Join.style</div>
-  )
-}
--- src/Pages/LoginPage/Login.jsx
+++ src/Pages/LoginPage/Login.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
+import SNSLoginSection from '../../Components/Login/SnsLogin';
+import styles from './Login.style.css';
 
-export default function Login() {
-  return (
-    <div>Login</div>
-  )
+function LoginPage({ handlePage }) {
+    return (
+        <div className={styles.mainLoginPage}>
+            <h1>반결고리</h1>
+            
+            {/* SNS 로그인  */}
+            <SNSLoginSection /> 
+
+            {/* 이메일로그인 회원가입 */}
+            <div className={styles.buttonGroup}>
+                <button className={`${styles.loginButton} ${styles.email}`} onClick={() => handlePage('login')}>
+                    이메일 로그인
+                </button>
+                <div className={styles.divider}>|</div> 
+                <button className={`${styles.loginButton} ${styles.signup}`} onClick={() => handlePage('join')}>
+                    회원가입
+                </button>
+            </div>
+        </div>
+    );
 }
+
+export default LoginPage;
