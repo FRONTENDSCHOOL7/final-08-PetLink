2a6b9f5355b70a96c867c0514ca03f523ed7d5a9
sejung.seo <ssj1565@naver.com>
2023-10-25 10:08:58 +0900

    :sparkles: [Add] BottomModal 생성

---
+++ src/Components/Common/Modal/BottomModal.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import styled, { keyframes } from 'styled-components'
+import { GlobalStyle } from '../../../Styles/reset.style'
+
+const slideUp = keyframes`
+  from {
+    transform: translateY(100%); // 처음 상태: 화면 아래
+  }
+  to {
+    transform: translateY(0);    // 최종 상태: 원래 위치
+  }
+`;
+
+const BottomModalContainer = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 92px;
+  padding: 1em 2em;
+  background-color: white;
+  border: 1px solid #e9e9e9;
+  border-radius: 10px 10px 0 0;
+  display: flex;
+  z-index: 2000;
+
+  animation: ${slideUp} 0.2s ease-out forwards;
+`
+
+const Report = styled.p`
+  color: red;
+  font-size: 14px;
+  position: relative;
+  padding-top: 20px;
+  display: inline-block;
+  width: 100%;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    height: 3px;
+    width: 100px;
+    background-color: #cccccc;
+  }
+`
+
+
+export default function BottomModal() {
+  return (
+    <>
+      <GlobalStyle/>
+      <BottomModalContainer>
+        <Report>신고하기</Report>
+      </BottomModalContainer>
+    </>
+  )
+}
