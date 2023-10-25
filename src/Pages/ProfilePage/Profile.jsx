bad0809f080b4e298ac6bf97ad783f7e46861366
maskelog <blitzmaske@gmail.com>
2023-10-25 09:49:23 +0900

    :memo: [Modify] 프로파일페이지추가

--- src/Pages/ProfilePage/Profile.jsx
+++ src/Pages/ProfilePage/Profile.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-export default function Profile() {
-  return (
-    <div>Profile</div>
-  )
-}
+const ProfilePage = () => {
+    const [profileData, setProfileData] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const token = localStorage.getItem('token'); // Get token directly from localStorage
+    
+                const response = await axios.get('https://api.mandarin.weniv.co.kr/user/myinfo', {
+                    headers: {
+                        Authorization: token ? `Bearer ${token}` : null,
+                        'Content-type': 'application/json',
+                    },
+                });
+    
+                // Check if the expected data structure is received
+                if (response.data && response.data[0] && response.data[0].user) {
+                    setProfileData(response.data[0].user);
+                } else {
+                    setError(new Error('Unexpected response format'));
+                }
+            } catch (error) {
+                setError(error);
+            }
+        };
+    
+        fetchData();
+    }, []);    
+
+    if (error) {
+        return <div>Error occurred: {error.message}</div>;
+    }
+
+    if (!profileData) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            <h1>Profile Page</h1>
+            <img src={profileData.image} alt="Profile" />
+            <h2>{profileData.username} (@{profileData.accountname})</h2>
+            <p>Followers: {profileData.followerCount}</p>
+            <p>Following: {profileData.followingCount}</p>
+            {/* Add more profile information here as needed */}
+        </div>
+    );
+};
+
+export default ProfilePage;
