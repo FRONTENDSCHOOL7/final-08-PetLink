3c0622f9ffc264fd5727e092e80d1491b183f503
maskelog <blitzmaske@gmail.com>
2023-10-30 16:08:25 +0900

    :memo: [Modify] 홈피드 포스트 상세보기 수정 & 라우터 수정

--- src/Components/Home/PostDetail.jsx
+++ src/Components/Home/PostDetail.jsx
@@ -1,134 +1,96 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import * as S from '../Home/PostList.style';
 import moreIcon from '../../assets/image/icon-more-vertical.png';
 import profileIcon from '../../assets/image/icon-basic-profile.png';
-import PostList, { PostListItem } from '../Home/PostList';
-import { useLocation } from 'react-router-dom'; // useLocation을 불러옵니다
 import { Container } from '../../Styles/reset.style';
 import HeaderLayouts from '../Common/Header/Header';
 import { Overlay } from '../Product/ProductDetail.style';
 import BottomModal from '../Common/Modal/BottomModal';
 import redHeartIcon from '../../assets/image/icon-heart-red.png';
 import commentIcon from '../../assets/image/icon-comment.png';
-import axios from 'axios';
-
 
 export default function PostDetail() {
   const [likeNum, setLikeNum] = useState(0);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [comment, setComment] = useState(''); 
-   // useLocation을 사용하여 현재 위치 정보를 가져옵니다.
-   const location = useLocation();
-   const selectedPost = location.state.selectedPost;
 
-
-  const onChangeModal = () => {
-    setIsModalOpen(true);
-  };
-
-
-  // const sendCommentToServer = async (commentContent, token) => {
-  //   try {
-  //     const apiUrl = `https://api.mandarin.weniv.co.kr/post/${selectedPost.id}/comments`;
-  //     const headers = {
-  //       'Authorization': `Bearer ${localStorage.getItem('token')}`,
-  //       'Content-Type': 'application/json; charset=utf-8', // 문자 인코딩 지정
-  //     };
-  //     const commentData = {
-  //       comment: {
-  //         content: commentContent,
-  //       },
-  //     };
-
-  //     const response = await axios.post(apiUrl, JSON.stringify(commentData), { headers });
-
-  //     if (response.status === 200) {
-  //       console.log('댓글이 성공적으로 서버로 전송되었습니다.');
-  //     } else {
-  //       console.error('댓글 서버로 전송 중 오류:', response.data);
-  //     }
-  //   } catch (error) {
-  //     console.error('댓글 서버로 전송 중 오류:', error);
-  //   }
-  // };
-
-  // const submitComment = (e) => {
-  //   e.preventDefault();
-  //   sendCommentToServer(comment, `Bearer ${localStorage.getItem('token')}`); // 토큰을 적절히 제공해야 합니다.
-  // };
-
-
+  const location = useLocation();
+  const { selectedPost } = location.state;
 
   if (!selectedPost) {
-    // 선택한 게시글이 없을 때의 처리
     return <div>게시글을 불러오는 중...</div>;
   }
-    return (
-      <Container>
-          <HeaderLayouts back search/>
- <S.UserInfo>
+
+  return (
+    <Container>
+      <HeaderLayouts back search />
+      <S.UserInfo>
         <S.UserProfile>
           <img src={profileIcon} alt='사용자 프로필 이미지' />
           <S.UserName>
-            <p>{selectedPost?.author?.username}</p>
-            <p>{selectedPost?.author?.accountname}</p>
+            <p>{selectedPost.author?.username}</p>
+            <p>{selectedPost.author?.accountname}</p>
           </S.UserName>
         </S.UserProfile>
       </S.UserInfo>
       <S.Content>
-        <p className='text'>{selectedPost?.content}</p>
-        {selectedPost?.image && <img src={selectedPost?.image} alt="포스팅 이미지" />}
+        <p className='text'>{selectedPost.content}</p>
+        {selectedPost.image && <img src={selectedPost.image} alt="포스팅 이미지" />}
       </S.Content>
       <S.PostIcons>
-        <button onClick={() => setLikeNum(likeNum + 1)}>
+        <button onClick={() => setLikeNum(prev => prev + 1)}>
           <img src={redHeartIcon} alt='좋아요 버튼' />
           <span>{likeNum}</span>
         </button>
-        <button onClick={onChangeModal}>
+        <button onClick={() => setIsModalOpen(true)}>
           <img src={commentIcon} alt='댓글 개수' />
         </button>
       </S.PostIcons>
-          {isModalOpen && (
-            <>
-              <Overlay onClick={() => setIsModalOpen(false)} />
-              <BottomModal reportTxt={["신고"]} setIsModalOpen={setIsModalOpen} />
-            </>
+      {isModalOpen && (
+        <>
+          <Overlay onClick={() => setIsModalOpen(false)} />
+          <BottomModal reportTxt={["신고"]} setIsModalOpen={setIsModalOpen} />
+        </>
       )}
-      <WriteComment  comment={comment} setComment={setComment}/>
-      </Container>
-    )
-  }
+      <WriteComment comment={comment} setComment={setComment} />
+    </Container>
+  );
+}
 
+export function CommentList({ onChangeModal }) {
+  return (
+    <S.CommentBox>
+      <S.UserInfo>
+        <div>
+          <a href='#'>
+            <img src={profileIcon} alt='사용자 프로필 이미지' />
+          </a>
+          <p>서귀포시 무슨 농장 <span>· 5분 전</span></p>
+        </div>
+        <button onClick={onChangeModal}>
+          <img src={moreIcon} alt='신고하기 모달창 불러오기' />
+        </button>
+      </S.UserInfo>
+      <S.CommentTxt>게시글 답글 ~~ !! 최고최고</S.CommentTxt>
+    </S.CommentBox>
+  );
+}
 
-export const CommentList = (props) => {
-    return (
-      <S.CommentBox>
-        <S.UserInfo>
+export function WriteComment({ comment, setComment }) {
+  return (
+    <S.InputForm>
       <div>
-            <a href='#'><img src={profileIcon} alt='사용자 프로필 이미지' /></a>
-              <p>서귀포시 무슨 농장 <span>· 5분 전</span></p>
-              
+        <img src={profileIcon} alt="사용자 프로필" />
+        <input 
+          type="text" 
+          id="comment-input" 
+          placeholder="댓글 입력하기..." 
+          onChange={e => setComment(e.target.value)} 
+          value={comment || ''} 
+        />
       </div>
-        <button onClick={props.onChangeModal}><img src={moreIcon} alt='신고하기 모달창 불러오기'/></button>
-        </S.UserInfo>
-        <S.CommentTxt>게시글 답글 ~~ !! 최고최고</S.CommentTxt>
-      </S.CommentBox>
-    );
-  };
-
-export function WriteComment({ comment, setComment, submitComment }){
-
-  const inputComment= (e)=>{
-    setComment(e.target.value)
-  }
-
-    return(
-      <S.InputForm onSubmit={submitComment}>
-              <div >
-                <img src={profileIcon} alt="사용자 프로필"/>
-                <input type="text" id="comment-input" placeholder="댓글 입력하기..." onChange={inputComment} value={comment || ''} />
-              </div>
-              <button type="submit" disabled={!comment || comment.length === 0}>게시</button>
-      </S.InputForm>
-    )
-  }
+      <button type="submit" disabled={!comment || comment.trim().length === 0}>게시</button>
+    </S.InputForm>
+  );
+}
--- src/Components/Home/PostList.jsx
+++ src/Components/Home/PostList.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import * as S from './PostList.style';
-import moreIcon from '../../assets/image/icon-more-vertical.png';
-import redHeartIcon from '../../assets/image/icon-heart-red.png';
-import commentIcon from '../../assets/image/icon-comment.png';
-import TabMenu from '../Common/TabMenu/TabMenu';
-import { Container } from '../../Styles/reset.style';
-import HeaderLayouts from '../Common/Header/Header';
-import { Overlay } from '../Product/ProductDetail.style';
-import BottomModal from '../Common/Modal/BottomModal';
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import * as S from "./PostList.style";
+import moreIcon from "../../assets/image/icon-more-vertical.png";
+import redHeartIcon from "../../assets/image/icon-heart-red.png";
+import commentIcon from "../../assets/image/icon-comment.png";
+import TabMenu from "../Common/TabMenu/TabMenu";
+import { Container } from "../../Styles/reset.style";
+import HeaderLayouts from "../Common/Header/Header";
+import { Overlay } from "../Product/ProductDetail.style";
+import BottomModal from "../Common/Modal/BottomModal";
 
 function formatDate(dateString) {
-  const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+  const options = { year: "numeric", month: "2-digit", day: "2-digit" };
   return new Date(dateString).toLocaleDateString(undefined, options);
 }
 
@@ -27,10 +27,10 @@ export default function PostList(props) {
   const fetchPostList = async () => {
     try {
       const response = await fetch(`https://api.mandarin.weniv.co.kr/post`, {
-        method: 'GET',
+        method: "GET",
         headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-          'Content-type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-type": "application/json",
         },
       });
       const data = await response.json();
@@ -41,7 +41,7 @@ export default function PostList(props) {
         setPosts([]);
       }
     } catch (error) {
-      console.error('에러:', error);
+      console.error("에러:", error);
     }
   };
 
@@ -49,13 +49,17 @@ export default function PostList(props) {
     navigate(`/post/${post._id}`);
   };
 
+
   return (
     <>
       <Container>
         <HeaderLayouts title="반결고리" logo={true} search />
         {posts.map((post, index) => (
-          <div key={index} >
-            <PostListItem post={post} onProfileClick={() => navigate(`/profile/${post.author._id}`)} />
+          <div key={index}>
+            <PostListItem
+              post={post}
+              onProfileClick={() => navigate(`/profile/${post.author._id}`)}
+            />
           </div>
         ))}
       </Container>
@@ -66,30 +70,32 @@ export default function PostList(props) {
 
 export function PostListItem({ post }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [accountname, setAccountName] = useState('');
-  const [username, setUserName] = useState('');
+  const [accountname, setAccountName] = useState("");
+  const [username, setUserName] = useState("");
   const [userImg, setUserImg] = useState(null);
   const [contentImgUrl, setContentImgUrl] = useState(null);
-  const [content, setContent] = useState('');
+  const [content, setContent] = useState("");
   const [likeNum, setLikeNum] = useState(0);
-  const [date, setDate] = useState('');
+  const [date, setDate] = useState("");
   const navigate = useNavigate();
 
-  // const handlePostClick = (post) => {
-  //   navigate(`/post/${post._id}`);
-  // };
+  const handlePostClick = (post) => {
+    navigate(`/post/${post._id}`);
+  };
 
   useEffect(() => {
     if (post) {
-      setAccountName(post.author.accountname || '');
-      setUserName(post.author.username || '');
-      setUserImg(post.author.image || 'https://api.mandarin.weniv.co.kr/Ellipse.png');
-      setContentImgUrl(post.image || '');
-      setContent(post.content || '');
-      setDate(post.createdAt || '');
+      setAccountName(post.author.accountname || "");
+      setUserName(post.author.username || "");
+      setUserImg(
+        post.author.image || "https://api.mandarin.weniv.co.kr/Ellipse.png"
+      );
+      setContentImgUrl(post.image || "");
+      setContent(post.content || "");
+      setDate(post.createdAt || "");
     }
   }, [post]);
-console.log()
+  console.log();
   const onChangeNum = () => {
     setLikeNum(likeNum + 1);
   };
@@ -102,7 +108,10 @@ console.log()
     <>
       <S.UserInfo>
         <S.UserProfile>
-          <Link to={`/profile/${post.author.accountname}`} state={{ selectedPost: post }}>
+          <Link
+            to={`/profile/${post.author.accountname}`}
+            state={{ selectedPost: post }}
+          >
             <img src={userImg} alt="사용자 프로필 이미지" />
           </Link>
           <S.UserName>
@@ -116,7 +125,7 @@ console.log()
       </S.UserInfo>
 
       <S.Content>
-        <Link to={`/post/detail/${post._id}`}  state={{ selectedPost: post }}>
+        <Link to={`/post/${post._id}`} state={{ selectedPost: post }}>
           <p className="text">{content}</p>
           {contentImgUrl && <img src={contentImgUrl} alt="포스팅 이미지" />}
         </Link>
@@ -125,7 +134,7 @@ console.log()
             <img src={redHeartIcon} alt="하트 아이콘" />
             <span>{likeNum}</span>
           </button>
-          <Link to={`/post/detail/${post._id}`}   state={{ selectedPost: post }}>
+          <Link to={`/post/detail/${post._id}`} state={{ selectedPost: post }}>
             <img src={commentIcon} alt="댓글 개수" />
             <span>1</span>
           </Link>
