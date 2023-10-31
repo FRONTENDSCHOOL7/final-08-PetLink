da1fdd91a470f5bb4efeb1474131dbcd74a63a7e
sunny <sunnya2023@gmail.com>
2023-11-01 00:55:32 +0900

    :memo: [Modify] 게시글, 댓글별 유저 프로필 설정

--- src/Components/Home/CommentList.jsx
+++ src/Components/Home/CommentList.jsx
@@ -5,6 +5,37 @@ import { useParams } from 'react-router-dom';
 
 export default function CommentList(props) {
   const defaultUserImg = "https://api.mandarin.weniv.co.kr/1698653743844.jpg";
+  const [comments, setComments] = useState([]);
+  const [userImg, setUserImg] = useState(null);
+  const { postId } = useParams();
+
+  useEffect(() => {
+    fetchMyProfile();
+  }, []);
+
+  const fetchMyProfile = async () => {
+    try {
+      const response = await fetch(`https://api.mandarin.weniv.co.kr/user/myinfo`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-type": "application/json",
+        },
+      });
+      const data = await response.json();
+
+      if (data) {
+        setUserImg(data.user.image);
+      }
+    } catch (error) {
+      console.error("에러:", error);
+    }
+  };
+
+  const addComment = (newComment) => {
+    setComments((prevComments) => [...prevComments, newComment]);
+  };
+
     if (!props.comment) {
       return null;
     }
@@ -13,7 +44,7 @@ export default function CommentList(props) {
         <S.UserInfo>
           <div>
             <a href='#'>
-              <img src={props.userImage  || defaultUserImg} alt='사용자 프로필 이미지' />
+              <img src={userImg  || defaultUserImg} alt='사용자 프로필 이미지' />
             </a>
             <p>{props.username} <span>· {props.date}</span></p>
           </div>
@@ -26,7 +57,7 @@ export default function CommentList(props) {
     );
   }
   
-  export function WriteComment({ comment, setComment, handlePostComment }) {
+  export function WriteComment({ comment, setComment, handlePostComment  }) {
   const [userImg, setUserImg] = useState(null);
   const { postId } = useParams();
   const defaultUserImg = "https://api.mandarin.weniv.co.kr/1698653743844.jpg";
@@ -57,7 +88,9 @@ export default function CommentList(props) {
 
   const postComment = async () => {
     try {
- 
+      if (typeof comment !== 'string') {
+        comment = '';
+      }
       const response = await fetch(`https://api.mandarin.weniv.co.kr/post/${postId}/comments`, {
         method: "POST",
         headers: {
@@ -73,16 +106,16 @@ export default function CommentList(props) {
 
       const responseData = await response.json();
 console.log(responseData)
-      if (responseData.comment) {
-       
+
+      if (responseData.comment  && responseData.comment.content) {
         setComment(responseData.comment.content);
       }
     } catch (error) {
-      console.error("에러:", error);
+      console.error("댓글 가져오기 에러:", error);
     }
   };
   console.log(comment)
-  
+
   return (
     <S.InputForm>
       <div>
