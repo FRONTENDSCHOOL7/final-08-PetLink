ebb93fd2ea35d94beb261d5bf00f53cceb73dad5
maskelog <blitzmaske@gmail.com>
2023-11-02 13:51:09 +0900

    :memo: [Modify] 프로필 개월 일자, 지역 추가 undefined 해결해야함

--- src/Pages/ProfilePage/Profile.jsx
+++ src/Pages/ProfilePage/Profile.jsx
@@ -22,6 +22,47 @@ const ProfilePage = () => {
     const [profileData, setProfileData] = useState(null);
     const [error, setError] = useState(null);
     const { accountname } = useParams(); 
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             const token = localStorage.getItem('token');
+    //             let url = 'https://api.mandarin.weniv.co.kr/user/myinfo';
+                
+    //             if (accountname) { 
+    //                 url = `https://api.mandarin.weniv.co.kr/profile/${accountname}`;
+    //             }
+    
+    //             const response = await axios.get(url, {
+    //                 headers: {
+    //                     Authorization: token ? `Bearer ${token}` : null,
+    //                     'Content-type': 'application/json',
+    //                 },
+    //             });
+            
+    //             let profile;
+    //             if (response.data.user) { // API에서 user가 있으면 (나의 프로필)
+    //                 profile = response.data.user;
+    //             } else if (response.data.profile) { // API에서 profile가 있으면 (사용자프로필)
+    //                 profile = response.data.profile;
+    //             } else {
+    //                 throw new Error('Unexpected response format');
+    //             }
+                
+    //             const introMatch = profile.intro.match(/#intro:(.*?)(?=#|$)/);
+    //             const introContent = introMatch ? introMatch[1].trim() : profile.intro;
+                
+    //             setProfileData({
+    //                 ...profile,
+    //                 intro: introContent
+    //             });
+    //         } catch (error) {
+    //             setError(error);
+    //         }
+    //     };
+    
+    //     fetchData();    
+    // }, [accountname]);
+
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -48,20 +89,34 @@ const ProfilePage = () => {
                     throw new Error('Unexpected response format');
                 }
                 
-                const introMatch = profile.intro.match(/#intro:(.*?)(?=#|$)/);
-                const introContent = introMatch ? introMatch[1].trim() : profile.intro;
+                const parsedIntro = parseIntro(profile.intro);
                 
                 setProfileData({
                     ...profile,
-                    intro: introContent
+                    ...parsedIntro
                 });
             } catch (error) {
                 setError(error);
             }
         };
     
-        fetchData();    
+        fetchData();
     }, [accountname]);
+    
+    function parseIntro(intro) {
+        const tags = ['intro', 'pet', 'gender', 'birthdate', 'location'];
+        const info = {};
+        
+        tags.forEach(tag => {
+            const match = intro.match(new RegExp(`#${tag}:(.*?)(?=#|$)`));
+            if (match && match[1]) {
+                info[tag] = match[1].trim();
+            }
+        });
+        
+        return info;
+    }
+    
 
     if (error) {
         return <div>Error occurred: {error.message}</div>;
@@ -70,6 +125,40 @@ const ProfilePage = () => {
     if (!profileData) {
         return <div>로딩...</div>;
     }
+    
+    // intro 정보 추출
+    function Tag({ label, value }) {
+        return value ? <p>{label}: {value}</p> : null;
+    }
+    
+    function ProfileDetails({ intro }) {
+        const info = parseIntro(intro);
+    
+        return (
+            <div>
+                {Object.entries(info).map(([key, value]) => <Tag key={key} label={capitalizeFirstLetter(key)} value={value} />)}
+            </div>
+        );
+    }
+    
+    function parseIntro(intro) {
+        const tags = ['intro', 'pet', 'gender', 'birthdate', 'location'];
+        const info = {};
+    
+        tags.forEach(tag => {
+            const match = intro.match(new RegExp(`#${tag}:(.*?)(?=#|$)`));
+            if (match && match[1]) {
+                info[tag] = match[1].trim();
+            }
+        });
+    
+        return info;
+    }
+    
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    
 
     // 팔로우 언팔 기능
 
@@ -82,13 +171,17 @@ const ProfilePage = () => {
                 },
             });
             if (response.data.profile) {
-                setProfileData(response.data.profile);
+                setProfileData(prevData => ({
+                    ...prevData, // 기존 데이터 유지
+                    ...response.data.profile, // 새로운 데이터로 업데이트
+                    isfollow: true // 팔로우 상태 업데이트
+                }));
             }
         } catch (error) {
             console.error(error);
         }
     };
-
+    
     const handleUnfollow = async () => {
         try {
             const response = await axios.delete(`https://api.mandarin.weniv.co.kr/profile/${profileData.accountname}/unfollow`, {
@@ -98,13 +191,35 @@ const ProfilePage = () => {
                 },
             });
             if (response.data.profile) {
-                setProfileData(response.data.profile);
+                setProfileData(prevData => ({
+                    ...prevData, // 기존 데이터 유지
+                    ...response.data.profile, // 새로운 데이터로 업데이트
+                    isfollow: false // 언팔로우 상태 업데이트
+                }));
             }
         } catch (error) {
             console.error(error);
         }
     };
 
+    // birthdate를 개월 또는 일자로 변환
+    function calculateAge(birthdateStr) {
+        const birthdate = new Date(birthdateStr);
+        const today = new Date();
+
+        let months = (today.getFullYear() - birthdate.getFullYear()) * 12;
+        months -= birthdate.getMonth();
+        months += today.getMonth();
+        
+        if (months <= 0) {
+            const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+            const diffDays = Math.round(Math.abs((today - birthdate) / oneDay));
+            return `${diffDays}일`;
+        }
+        
+        return `${months}개월`;
+    }
+    
     return (
         <>
         <GlobalStyle/>
@@ -117,25 +232,36 @@ const ProfilePage = () => {
                 </FollowGroup>
                 
                 <ProfileImageContainer>
-                    <ProfileImage src={profileData.image} alt="Profile" />
-                    <ProfileUsername>{profileData.username} </ProfileUsername>
-                    <ProfileAccountname>@{profileData.accountname}</ProfileAccountname>
-                </ProfileImageContainer>
+                <ProfileImage src={profileData.image} alt="Profile" />
+                <ProfileUsername>{profileData.username} </ProfileUsername>
+                <ProfileAccountname>@{profileData.accountname}</ProfileAccountname>
+                <ProfileAccountname>
+                    {profileData.gender && `${profileData.gender} `}
+                    {profileData.pet && `${profileData.pet} `}
+                    {profileData.birthdate && `${calculateAge(profileData.birthdate)} `}
+                    {profileData.location && `${profileData.location}`}
+                </ProfileAccountname>
+
+            </ProfileImageContainer>
                 
                 <FollowGroup>
                     <FollowCount>{profileData.followerCount}</FollowCount>
                     <FollowLabel>Followers</FollowLabel>
                 </FollowGroup>
             </FollowInfo>
-
-            <ProfileIntro>{profileData.intro}</ProfileIntro>
+    
+            {/* 버튼 myinfo / userprofile에 따라 버튼 변경 */}
             {accountname ? (
             profileData.isfollow ? (
-                <BtnGroup>
+                <BtnGroup style={{
+                    marginRight: "10px"
+                }}>
                     <Button onClick={handleUnfollow}>언팔로우</Button>
                 </BtnGroup>
             ) : (
-                <BtnGroup>
+                <BtnGroup style={{
+                    marginRight: "10px"
+                }}>
                     <Button onClick={handleFollow}>팔로우</Button>
                 </BtnGroup>
             )
@@ -153,7 +279,7 @@ const ProfilePage = () => {
         </Container>
         <TabMenu/>
         </>
-    );
+    );    
 };
 
 export default ProfilePage;
