ab40080a16fe66906c563c7e48476cfacb7e789f
CreateSong <thdcodbs1@naver.com>
2023-12-20 13:46:17 +0900

    :fire: [RemoveC] 하드코딩된 코드 삭제

--- src/Pages/CommunityPage/Community.jsx
+++ src/Pages/CommunityPage/Community.jsx
@@ -14,9 +14,6 @@ import iconMap from '../../assets/image/icon-map.png';
 import shareInfoMap from '../../assets/image/img-share-info-map.png';
 import walkingCrewMap from '../../assets/image/img-walking-crew-map.png';
 import missingReportMap from '../../assets/image/img-missing-report-map.png';
-import userProfile from '../../assets/image/icon-basic-profile.png';
-import userProfile2 from '../../assets/image/icon-test-user-profile.png';
-import userProfile3 from '../../assets/image/icon-test-user-profile2.png';
 import addBtn from '../../assets/image/icon-add.png';
 import Loading from '../../Components/Common/Modal/Loading';
 
@@ -76,109 +73,6 @@ function Community() {
     '반려 돌보미': shareInfoMap,
     '실종 신고': missingReportMap,
   };
-
-  // const contentData = {
-  //   '정보 공유': {
-  //     mapImage: shareInfoMap,
-  //     posts: [
-  //     {
-  //       profileImage: userProfile,
-  //       title: '광화문 24시 동물병원 추천!',
-  //       author: '김펫피',
-  //       likes: 0,
-  //       comments: 0
-  //     },
-  //     {
-  //       profileImage: userProfile2, // 이미지와 내용을 원하는 대로 수정하세요.
-  //       title: '종각역 애견카페 추천드려요!',
-  //       author: '애견카페러버',
-  //       likes: 5,
-  //       comments: 2
-  //     },
-  //     {
-  //       profileImage: userProfile3, // 이미지와 내용을 원하는 대로 수정하세요.
-  //       title: '시청역 강아지 동반 음식점',
-  //       author: '펫피플',
-  //       likes: 3,
-  //       comments: 1
-  //     }
-  //   ]
-  //   },
-
-  //   '산책 크루': {
-  //     mapImage: walkingCrewMap,  // 이미지 경로를 수정해주세요.
-  //     posts: [
-  //     {
-  //       profileImage: userProfile2,
-  //       title: '내일 오후 8시 광화문 산책 같이하실 분~',
-  //       author: '산책매니아',
-  //       likes: 0,
-  //       comments: 0
-  //     },
-  //     {
-  //       profileImage: userProfile3,
-  //       title: '대형견 산책 함께 하실 분~',
-  //       author: '리트리버',
-  //       likes: 0,
-  //       comments: 0
-  //     },
-  //     {
-  //       profileImage: userProfile,
-  //       title: '힙지로 산책 크루 모집합니다!',
-  //       author: '홍길동',
-  //       likes: 0,
-  //       comments: 0
-  //     }
-  //   ]
-  //   },
-    
-  //   '반려 돌보미': {
-  //     mapImage: shareInfoMap,  // 이미지 경로를 수정해주세요.
-  //     posts: [
-  //     {
-  //       profileImage: userProfile3,
-  //       title: '광화문/종각/시청/을지로 반려동물 대신 봐드려요~',
-  //       author: '김시루',
-  //       likes: 0,
-  //       comments: 0
-  //     },
-  //     {
-  //       profileImage: userProfile,
-  //       title: '내일 하루만 강아지 돌봐주실 분ㅠ',
-  //       author: '박감자',
-  //       likes: 0,
-  //       comments: 0
-  //     },
-  //     {
-  //       profileImage: userProfile2,
-  //       title: '내일 하루만 강아지 돌봐주실 분ㅠ',
-  //       author: '김뽀삐',
-  //       likes: 0,
-  //       comments: 0
-  //     }
-  //   ]
-  //   },
-
-  //   '실종 신고': {
-  //     mapImage: missingReportMap,  // 이미지 경로를 수정해주세요.
-  //     posts: [
-  //     {
-  //       profileImage: userProfile3,
-  //       title: '강아지를 찾습니다ㅠㅠ(광화문 근처)',
-  //       author: '중구시민',
-  //       likes: 0,
-  //       comments: 0
-  //     },
-  //     {
-  //       profileImage: userProfile2,
-  //       title: '시청역 근처에서 돌아다니는 시츄 보신 분!!',
-  //       author: '김시츄',
-  //       likes: 0,
-  //       comments: 0
-  //     }
-  //   ]
-  //   },
-  // };
 
   const currentMapImage = mapImages[activeCategory];
 
